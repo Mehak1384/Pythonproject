4d4653c2a02ae634e094e9382943c8c6fe90c39a
Mehak <mehakarora1348@gmail.com>
2026-07-21 10:07:55 +0530

    Emp input

---
+++ EMPLOYEE REGISTRATION SYSTEM/empolyeedetails.py
@@ -0,0 +1,8 @@
+def details(Empname,Empage,Empposition,Empcode,Empexperince,Empqulation):
+    Empcode=int(input("Enter your Employee code"))
+    Empname=input("Enter the Employee name :")
+    Empage=input("Enter your age in form Date.Month.Year")
+    Empqulation=input("Enter your Education Level")
+    Empexperince=float(input("Enter your Exp. in terms of years"))
+    Empposition=input("Enter you position in Company")
+details()
