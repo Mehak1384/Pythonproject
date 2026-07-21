c9a63c2d841824df568b61a11e354b6d8033e663
Mehak <mehakarora1348@gmail.com>
2026-07-21 10:33:27 +0530

    Updated code

--- EMPLOYEE REGISTRATION SYSTEM/empolyeedetails.py
+++ EMPLOYEE REGISTRATION SYSTEM/empolyeedetails.py
@@ -1,8 +1,9 @@
-def details(Empname,Empage,Empposition,Empcode,Empexperince,Empqulation):
+def details():
     Empcode=int(input("Enter your Employee code"))
     Empname=input("Enter the Employee name :")
     Empage=input("Enter your age in form Date.Month.Year")
     Empqulation=input("Enter your Education Level")
     Empexperince=float(input("Enter your Exp. in terms of years"))
     Empposition=input("Enter you position in Company")
+    return Empname,Empage,Empposition,Empcode,Empexperince,Empqulation
 details()
--- EMPLOYEE REGISTRATION SYSTEM/main.py
+++ EMPLOYEE REGISTRATION SYSTEM/main.py
@@ -1,4 +1,11 @@
 from empolyeedetails import details
+from empdisplay import emdisplay
 print("Welcome to Employee Registration System")
-details()
-
+count=0
+while True:
+    Empname,Empage,Empposition,Empcode,Empexperince,Empqulation=details()
+    count=+1
+    choice=input("Enter you want to enter the more Emp")
+    if choice== "N" or choice=="n":
+        break
+emdisplay(Empname,Empage,Empposition,Empcode,Empexperince,Empqulation)
