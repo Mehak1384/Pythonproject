9bd58d479f0325ff6b71cc7ac6c2807f17d8ad1c
Mehak <mehakarora1348@gmail.com>
2026-07-24 11:47:16 +0530

    New function is added in the code to get the proper result

--- PracticecodebyHackerrank/Splitandjoin.py
+++ PracticecodebyHackerrank/Splitandjoin.py
@@ -6,4 +6,12 @@ def join(str):
     str="-".join(str)
     return str
 print(join(str))
+"""Given above code output is ['this', 'is', 'a', 'string']
+ -t-h-i-s- -i-s- -a- -s-t-r-i-n-g- - -  and our required output is this this-is-a-string"""
+def split_and_join(line):
+     str=line.split()
+     str ="-".join(str)
+     return str
+print(split_and_join(str))
+
 
