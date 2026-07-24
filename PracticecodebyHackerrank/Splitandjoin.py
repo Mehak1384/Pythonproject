24965e322f11bad879f70173c32fa9f9b117fa99
Mehak <mehakarora1348@gmail.com>
2026-07-24 11:37:23 +0530

    Code written

---
+++ PracticecodebyHackerrank/Splitandjoin.py
@@ -0,0 +1,9 @@
+str=input("Enter the string you want to split and join")
+def split(str):
+    return str.split()
+print(split(str))
+def join(str):
+    str="-".join(str)
+    return str
+print(join(str))
+
