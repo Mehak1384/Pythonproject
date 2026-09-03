de1ea7bfde767e65d0e80f92158db6f4c01b9aa4
mehak1348 <mehak1348@gmail.com>
2026-09-03 10:27:38 +0530

    Question 3 of array of dsa

--- PracticecodebyHackerrank/DSA/PracticeDay1.py
+++ PracticecodebyHackerrank/DSA/PracticeDay1.py
@@ -22,15 +22,19 @@ Prints the element at that index.
 Handles an invalid index without crashing."""
 arr = [15, 25, 35, 45, 55]
 index=int(input("Please provide us index you want to print"))
-if index==0:
+if index>=0 and index<len((arr)):
     print(arr[index])
-elif index==1:
-    print(arr[index])
-elif index==2:
-    print(arr[index])
-elif index==3:
-    print(arr[index])
-elif index==4:
-    print(arr[index])
-else :
-    print("Sorry we can find the value for the given index",index)
+else:
+   print("Sorry we can find the value for the given index",index)
+"""Q3. Index and Value
+Given:
+arr = [10, 20, 30, 40, 50]
+Traverse the list and print:
+Index 0 -> 10
+Index 1 -> 20
+Index 2 -> 30
+Index 3 -> 40
+Index 4 -> 50"""
+arr = [10, 20, 30, 40, 50]
+for i in range(len(arr)):
+    print("Index",i,"->",arr[i])
