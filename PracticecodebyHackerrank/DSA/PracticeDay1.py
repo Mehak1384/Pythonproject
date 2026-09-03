624ce5f4ce833f5450823e5d07939f58cf6fb716
mehak1348 <mehak1348@gmail.com>
2026-09-03 11:49:11 +0530

    q4,5

--- PracticecodebyHackerrank/DSA/PracticeDay1.py
+++ PracticecodebyHackerrank/DSA/PracticeDay1.py
@@ -38,3 +38,61 @@ Index 4 -> 50"""
 arr = [10, 20, 30, 40, 50]
 for i in range(len(arr)):
     print("Index",i,"->",arr[i])
+"""Write a program to traverse:
+arr = [12, 24, 36, 48, 60]
+and print every element without using its index."""
+arr = [12, 24, 36, 48, 60]
+for elements in arr:
+    print(elements)
+"""Q5.Find the Largest Element ⭐
+Given:
+
+arr = [12, 45, 7, 89, 34]
+
+Find and print the largest element.
+
+Restrictions:
+
+Don't use max()
+Use a loop."""
+arr=[12,54,7,89,34]
+max_n=0
+for i in range(len(arr)):
+        if max_n<arr[i]:
+            max_n=arr[i]
+        
+print(max_n)
+"""Q4. Find the Smallest Element ⭐
+Given:
+
+arr = [25, 8, 42, 3, 19]
+
+Find and print the smallest element.
+
+Restrictions:
+
+Don't use min()
+Use a loop."""
+arr = [25, 8, 42, 3, 19]
+min_n=arr[0]
+for i in range(len(arr)):
+        if min_n>arr[i]:
+            min_n=arr[i]
+        
+print(min_n)
+"""Q5. Count Even and Odd Elements ⭐
+Take n integers from the user and count how many elements are even and how many are odd."""
+n=int(input("Enter the elemensr you wan to check "))
+arr=[0]*n
+for i in range(len(arr)):
+    arr[i]=int(input())
+count=0
+count_odd=0
+for i in range(len(arr)):
+    if(arr[i]%2!=0):
+        count+=1
+    else:
+        count_odd+=1
+print(count,count_odd)
+for i in range(len(arr)):
+    print(i,arr[i])
