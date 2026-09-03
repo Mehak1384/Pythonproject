564869a8a3713b2c85992ce1461c8c4175b66052
mehak1348 <mehak1348@gmail.com>
2026-09-03 10:21:46 +0530

    added 2nd program of dsa

--- PracticecodebyHackerrank/DSA/PracticeDay1.py
+++ PracticecodebyHackerrank/DSA/PracticeDay1.py
@@ -13,4 +13,24 @@ for i in range(n):
 print("Traverse of an array")
 for i in range(n):
     print(i,arr[i])
-
+"""Q2. Access by Index
+Given:
+arr = [15, 25, 35, 45, 55]
+Write a program that:
+Takes an index from the user.
+Prints the element at that index.
+Handles an invalid index without crashing."""
+arr = [15, 25, 35, 45, 55]
+index=int(input("Please provide us index you want to print"))
+if index==0:
+    print(arr[index])
+elif index==1:
+    print(arr[index])
+elif index==2:
+    print(arr[index])
+elif index==3:
+    print(arr[index])
+elif index==4:
+    print(arr[index])
+else :
+    print("Sorry we can find the value for the given index",index)
